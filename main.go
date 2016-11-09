521ff6866bb863c2b8ab43c3090db05e7095a7ae
Jacob Barrieault <jacob@quikly.com>
2016-11-09 11:53:59 -0500

    Add deal flag, refactor blocking channel loop

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"strings"
 	"sync"
 	"time"
 )
@@ -17,7 +18,8 @@ type ConnManager struct {
 var (
 	numConn      = flag.Int("connections", 0, "number of concurrent connections")
 	numMsg       = flag.Int("messages", 5, "Number of messages to be broadcasted")
-	host         = flag.String("host", "ws://localhost:8081/ws?channelId=deal:228", "websocket server address")
+	host         = flag.String("host", "ws://localhost:8081/ws", "websocket server address")
+	deal         = flag.String("deal", "10107", "deal id")
 	done         = make(chan bool)
 	connections  = &ConnManager{conns: make(map[*Connection]bool)}
 	msgStats     = make(map[string]int64)
@@ -26,44 +28,44 @@ var (
 
 func main() {
 	flag.Parse()
+	dialAddr := strings.Join([]string{*host, "?channelId=deal:", *deal}, "")
 
-	if *numConn > 0 {
+	if !(*numConn > 0) {
+		return
+	}
 
-		var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-		for i := 0; i < *numConn; i++ {
-			wg.Add(1)
-			c := &Connection{id: i, send: make(chan []byte, 256)}
-
-			go func() {
-				c.dial(*host, &wg)
-			}()
-
-			if (i+1)%MAX_CONCURRENT_DIAL == 0 {
-				wg.Wait()
-			}
-		}
+	for i := 0; i < *numConn; i++ {
+		wg.Add(1)
+		c := &Connection{id: i, send: make(chan []byte, 256)}
 
 		go func() {
-			for {
-				log.Println("\n\nReceived messages: ")
-				for m, n := range msgStats {
-					log.Println(m, n)
-				}
-				time.Sleep(1 * time.Second)
-			}
+			c.dial(dialAddr, &wg)
 		}()
 
-		for {
-			select {
-			case m := <-receivedMsgs:
-				if _, ok := msgStats[m]; ok {
-					msgStats[m] += 1
-				} else {
-					msgStats[m] = 1
-				}
-			}
+		if (i+1)%MAX_CONCURRENT_DIAL == 0 {
+			wg.Wait()
 		}
+	}
 
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		for {
+			log.Println("\n\nReceived messages: ")
+			for m, n := range msgStats {
+				log.Println(m, n)
+			}
+			<-ticker.C
+		}
+	}()
+
+	for {
+		m := <-receivedMsgs
+		if _, ok := msgStats[m]; ok {
+			msgStats[m] += 1
+		} else {
+			msgStats[m] = 1
+		}
 	}
 }
