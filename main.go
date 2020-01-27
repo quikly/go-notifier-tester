0a6afeed761820eff6b6d647006a6f5553f6dbcd
Jake Wesorick <jake@wesorick.dev>
2020-01-27 15:37:27 -0500

    WS client to work with ActionCable + GraphQL subscriptions in the main app.

--- main.go
+++ main.go
@@ -1,71 +1,127 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
 	"flag"
 	"log"
-	"strings"
+	"net/url"
+	"os"
+	"os/signal"
+	"strconv"
 	"sync"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-const MAX_CONCURRENT_DIAL = 50
+var hostFlag = flag.String("host", "www.quikly.localhost:5000", "http service address")
+var numCon = flag.Int("numCon", 5, "max number of concurrent clients")
+var schemeFlag = flag.String("scheme", "ws", "ws or wss (like http or https)")
 
-type ConnManager struct {
-	sync.Mutex
-	conns map[*Connection]bool
-}
-
-var (
-	numConn      = flag.Int("connections", 0, "number of concurrent connections")
-	numMsg       = flag.Int("messages", 5, "Number of messages to be broadcasted")
-	host         = flag.String("host", "ws://localhost:8081/ws", "websocket server address")
-	deal         = flag.String("deal", "10107", "deal id")
-	done         = make(chan bool)
-	connections  = &ConnManager{conns: make(map[*Connection]bool)}
-	msgStats     = make(map[string]int64)
-	receivedMsgs = make(chan string)
-)
+const subscribeJSON = `{"command":"subscribe","identifier":"{\"channel\":\"GraphQLChannel\",\"channelId\":\"16fe8fc2f8e\"}"}`
+const messageJSON = `{"command":"message","identifier":"{\"channel\":\"GraphQLChannel\",\"channelId\":\"16fe8fc2f8e\"}","data":"{\"query\":\"subscription getMessage {\\n  newMessage {\\n    message\\n    __typename\\n  }\\n}\\n\",\"variables\":{},\"operationName\":\"getMessage\",\"action\":\"execute\"}"}`
 
 func main() {
 	flag.Parse()
-	dialAddr := strings.Join([]string{*host, "?channelId=deal:", *deal}, "")
+	log.SetFlags(0)
 
-	if !(*numConn > 0) {
-		return
+	var waitGroup sync.WaitGroup
+	var max = *numCon
+
+	if len(os.Getenv("NUM_CON")) > 0 {
+		i, _ := strconv.ParseInt(os.Getenv("NUM_CON"), 10, 64)
+		max = int(i)
 	}
 
-	var wg sync.WaitGroup
-
-	for i := 0; i < *numConn; i++ {
-		wg.Add(1)
-		c := &Connection{id: i, send: make(chan []byte, 256)}
-
-		go func() {
-			c.dial(dialAddr, &wg)
-		}()
-
-		if (i+1)%MAX_CONCURRENT_DIAL == 0 {
-			wg.Wait()
-		}
+	for i := 0; i < max; i++ {
+		go createClient(&waitGroup, i)
 	}
+
+	waitGroup.Wait()
+}
+
+func createClient(waitGroup *sync.WaitGroup, clientID int) {
+	waitGroup.Add(1)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	host := *hostFlag
+
+	if len(os.Getenv("TARGET_HOST")) > 0 {
+		host = os.Getenv("TARGET_HOST")
+	}
+
+	scheme := *schemeFlag
+	if len(os.Getenv("SCHEME")) > 0 {
+		scheme = os.Getenv("SCHEME")
+	}
+
+	u := url.URL{Scheme: scheme, Host: host, Path: "/cable"}
+	log.Printf("client: %d connecting to %s", clientID, u.String())
+
+	c, res, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err, "response: ", res)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
 
 	go func() {
-		ticker := time.NewTicker(1 * time.Second)
+		defer close(done)
 		for {
-			log.Println("\n\nReceived messages: ")
-			for m, n := range msgStats {
-				log.Println(m, n)
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Printf("client: %d read:", clientID, err)
+				return
 			}
-			<-ticker.C
+			log.Printf("client: %d recv: %s", clientID, message)
 		}
 	}()
 
+	ticker := time.NewTicker(time.Second)
+	// defer ticker.Stop()
+
 	for {
-		m := <-receivedMsgs
-		if _, ok := msgStats[m]; ok {
-			msgStats[m] += 1
-		} else {
-			msgStats[m] = 1
+		select {
+		case <-done:
+			waitGroup.Done()
+			return
+		case <-ticker.C:
+
+			err := c.WriteMessage(websocket.TextMessage, []byte(subscribeJSON))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+
+			err = c.WriteMessage(websocket.TextMessage, []byte(messageJSON))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+
+			ticker.Stop()
+
+		case <-interrupt:
+			log.Printf("client: %d interrupt", clientID)
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Printf("client: %d write close:", clientID, err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			waitGroup.Done()
+			return
 		}
 	}
 }
